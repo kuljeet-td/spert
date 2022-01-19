4d9684e69b11d3dabffc9d0d93d7400c7eef4296
kuljeet-td <kuljeet.s@tatrasdata.com>
2022-01-19 16:35:46 +0530

    dockerfile

--- application.py
+++ application.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from spert.api.endpoint import keyphrases_bot
 
 application = Flask(__name__)
 application.config["JSON_SORT_KEYS"] = False
 
 application.register_blueprint(keyphrases_bot)
+
+
+@application.route("/")
+def index():
+    return jsonify({"message": "You see this message. The application is running on 8082."})
+
 
 if __name__ == "__main__":
     application.run(debug=False)
